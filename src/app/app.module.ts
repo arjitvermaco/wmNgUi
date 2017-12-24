2d9b0a13a3a5b17b6830714d66f1de470f30f130
arjitvermaco <swiftdesk.01@gmail.com>
2017-12-24 21:01:06 +0530

    basic structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {Routes, RouterModule} from '@angular/router';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './common/components/header/header.component';
+import { FooterComponent } from './common/components/footer/footer.component';
+import { HomeComponent } from './home/home/home.component';
+import { BannerComponent } from './home/banner/banner.component';
+import { CategoriesComponent } from './categories/categories/categories.component';
+import { ExpertsComponent } from './experts/experts.component';
+import { BlogsComponent } from './blogs/blogs.component';
+import { HowitworksComponent } from './howitworks/howitworks.component';
+
+const appRoutes:Routes = [
+  {path:'',component:HomeComponent},
+  {path:'experts',component:ExpertsComponent},
+  {path:'categories',component:CategoriesComponent},
+  {path:'how-it-works',component:HowitworksComponent},
+  {path:'blogs',component:BlogsComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    BannerComponent,
+    CategoriesComponent,
+    ExpertsComponent,
+    BlogsComponent,
+    HowitworksComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
